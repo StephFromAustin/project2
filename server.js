fc7ebff2ea7bd3bac32239487c2a0a27a9c78726
KSG Sounds Macbook Pro <ksgsound@StephGonza.local>
2018-11-13 20:13:00 -0600

    command line out handlebar depencdencies code in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES 
 require ("dotenv").config(); //.env
 const express = require("express"); //express  
-const exphbs = require("express-handlebars"); //handlebars
+// const exphbs = require("express-handlebars"); //handlebars
     // SETS UP THE EXPRESS APP 
 const db = require("./models"); // requires models for syncing 
 const app = express();
