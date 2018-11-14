0bdea1e7e45de5f510602efcfc6e960cae50318f
KSG Sounds Macbook Pro <ksgsound@StephGonza.local>
2018-11-13 20:11:58 -0600

    middleware added to server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 require ("dotenv").config(); //.env
 const express = require("express"); //express  
 const exphbs = require("express-handlebars"); //handlebars
-const db = require("./models");
+    // SETS UP THE EXPRESS APP 
+const db = require("./models"); // requires models for syncing 
 const app = express();
 const PORT = process.env.PORT || 8080; // PORT
 
 // MIDDLEWARE
+app.use(express.urlencoded({ extended:false })); // parse application 
+app.use(express.json()); // parse application/ json
+app.use(express.static("public")); // static directory 
