c58ea5e904b7a34210bd5cd155dc0bc13767433e
Stephanie Gonzales <stephgonza@utexas.edu>
2018-11-20 19:43:27 -0600

    heroku push fix, deleted original local host connection in connection.js file

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,13 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Steph2845",
-  database: "project2_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Steph2845",
+//   database: "project2_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
