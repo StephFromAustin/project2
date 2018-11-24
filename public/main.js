a3495fb43bbaa6674ea1ea8bef0a426b1547bb9d
motoko104 <motoko104@yahoo.com>
2018-11-23 19:58:28 -0600

    minor edits to html structures, mostly added functionality to firebase and added email verification as well as password reset functions

--- public/main.js
+++ public/main.js
@@ -1,27 +1,32 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const container = document.querySelector(".container2");
+$(document).ready(function () {
+  //CSS-Javascript scripts for page visual functionality
+  //============================================================================================
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+  const container = document.querySelector(".container2");
 
-// ScrollSpy
-const ss = document.querySelectorAll('.scrollspy');
-M.ScrollSpy.init(ss, {});
-//sidenav
-const sideNav = document.querySelector('.sidenav');
-M.Sidenav.init(sideNav, {});
+  // ScrollSpy
+  const ss = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(ss, {});
+  //sidenav
+  const sideNav = document.querySelector('.sidenav');
+  M.Sidenav.init(sideNav, {});
 
-left.addEventListener("mouseenter", () => {
-  container.classList.add("hover-left");
-});
+  //event listeners for mouse actions on elements
+  left.addEventListener("mouseenter", () => {
+    container.classList.add("hover-left");
+  });
 
-left.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-left");
-});
+  left.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-left");
+  });
 
-right.addEventListener("mouseenter", () => {
-  container.classList.add("hover-right");
-});
+  right.addEventListener("mouseenter", () => {
+    container.classList.add("hover-right");
+  });
 
-right.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-right");
-});
-
+  right.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-right");
+  });
+//======================================================================================================================
+})
