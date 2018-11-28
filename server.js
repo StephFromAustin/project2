c44a517ac50dec5f13146d9a2df7ff0cbafb6b04
Stephanie Gonzales <stephgonza@utexas.edu>
2018-11-27 20:01:35 -0600

    dotenv required in server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // DEPENDENCIES 
-//require ("dotenv").config(); //.env
-const express = require("express"); //express  
-
+    // DOTENV
+require("dotenv").config();
+    // EXPRESS 
+const express = require("express"); 
     // SETS UP THE EXPRESS APP 
-const db = require("./models"); // requires models for syncing 
+const db = require("./models");       // REQUIRES MODELS FOR SYNCING
 const app = express();
-const PORT = process.env.PORT || 8080; // PORT
+    // PORT
+const PORT = process.env.PORT || 8080; 
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended:false })); // parse application 
